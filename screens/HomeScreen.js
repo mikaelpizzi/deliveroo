1927ca3c282a4fe05c91b512113c3b2e8e94632b
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-05-29 00:31:46 -0400

    feat: Implement HomeScreen app header

---
+++ screens/HomeScreen.js
@@ -0,0 +1,52 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
+import Categories from "../components/categories";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+  return (
+    <SafeAreaView className="bg-white pt-5 mt-5">
+      {/* Header */}
+      <View className="flex flex-row pb-3 items-center mx-4 space-x-2 px-2">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00ccbb" />
+          </Text>
+        </View>
+
+        <UserIcon size={35} color="#00ccbb" />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
