4fe03fe2f5c33209f907f0da6de7a10a1ce41eb4
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-06-03 13:16:04 -0400

    feat: Implement category cards

--- components/Categories.js
+++ components/Categories.js
@@ -1,11 +1,54 @@
-import { View, Text } from "react-native";
+import { ScrollView } from "react-native";
 import React from "react";
+import CategoryCard from "./CategoryCard";
 
 const Categories = () => {
   return (
-    <View>
-      <Text>Categories</Text>
-    </View>
+    <ScrollView
+      contentContainerStyle={{
+        paddingHorizontal: 15,
+        paddingTop: 10,
+      }}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+    >
+      {/* Category Card */}
+
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="testing 1"
+      />
+
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="testing 2"
+      />
+
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="testing 3"
+      />
+
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="testing 4"
+      />
+
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="testing 5"
+      />
+
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="testing 6"
+      />
+
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="testing 7"
+      />
+    </ScrollView>
   );
 };
 
