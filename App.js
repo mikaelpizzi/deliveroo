322014fb50c8a571504ad285183ef84fa7f4f3fa
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-06-10 23:10:34 -0400

    add: Add Restaurant Screen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
+import RestaurantScreen from "./screens/RestaurantScreen";
 import "react-native-url-polyfill/auto";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Stack.Navigator>
         {/* Screens */}
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Restaurant" component={RestaurantScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
